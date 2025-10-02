ea1c1fbb029fd469279eb794d429fa3dd466f7fe
lanelyx <leave.m3@yandex.ru>
2025-10-02 20:49:59 +0300

    Знакомство с PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import expect, Page
 import pytest
+from pages.login_page import LoginPage
 
 authorization_data_email_pass = [
     ("user.name@gmail.com", "password"),
@@ -19,20 +20,26 @@ def unpackage_auth_data(authorization_data_email_pass):
 )
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
 
-    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill(email)
-
-    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill(password)
-
-    # login_button = page.locator('//button[@data-testid="login-page-login-button"]') # поиск по классическому локатору
-    login_button = chromium_page.get_by_test_id(
-        'login-page-login-button')  # поиск по data-testid(у Playwright нативная поддержка данного атрибута)
-    login_button.click()
-
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
+    #
+    # chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    #
+    # email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
+    # email_input.fill(email)
+    #
+    # password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
+    # password_input.fill(password)
+    #
+    # # login_button = page.locator('//button[@data-testid="login-page-login-button"]') # поиск по классическому локатору
+    # login_button = chromium_page.get_by_test_id(
+    #     'login-page-login-button')  # поиск по data-testid(у Playwright нативная поддержка данного атрибута)
+    # login_button.click()
+    #
+    # wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
+    # expect(wrong_email_or_password_alert).to_be_visible()
+    # expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
