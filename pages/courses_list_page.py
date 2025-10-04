b271aa0e810f54dc0e675bc680ea260c2e788070
lanelyx <leave.m3@yandex.ru>
2025-10-04 21:06:31 +0300

    Практикуемся в реализации PageComponent 3(видео)

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import Page, expect
 
 from components.courses.course_view_component import CourseViewComponent
+from components.courses.courses_list_toolbar_view_component import CourseListToolbarViewComponent
 from components.navigation.navbar_component import NavbarComponent
 from components.views.empty_view_component import EmptyViewComponent
 from pages.base_page import BasePage
@@ -10,27 +11,15 @@ from components.navigation.sidebar_component import SidebarComponent
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.toolbar_view = CourseListToolbarViewComponent(page)
         self.course_view = CourseViewComponent(page)
         self.empty_view = EmptyViewComponent(page, 'courses-list')
-
         self.navbar = NavbarComponent(page)
         self.sidebar = SidebarComponent(page)
-
-        self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
-        self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
-
-    def check_visible_courses_title(self):
-        expect(self.courses_title).to_be_visible()
-        expect(self.courses_title).to_have_text("Courses")
 
     def check_visible_empty_view(self):
         self.empty_view.check_visible(
             title="There is no results",
             description="Results from the load test pipeline will be displayed here"
         )
-
-    def check_visible_create_course_button(self):
-        expect(self.create_course_button).to_be_visible()
-
-    def click_create_course_button(self):
-        self.create_course_button.click()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -11,8 +11,7 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
     courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
     courses_list_page.navbar.check_visible('username')
     courses_list_page.sidebar.check_visible()
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.check_visible_empty_view()
 
 
@@ -47,8 +46,7 @@ def test_create_course(
         min_score='10'
     )
     create_courses_page.click_create_course_button()
-    courses_list_page.check_visible_courses_title()
-    courses_list_page.check_visible_create_course_button()
+    courses_list_page.toolbar_view.check_visible()
     courses_list_page.course_view.check_visible(
         index=0,
         title='Playwright',
