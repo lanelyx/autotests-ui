d60f52506e1634b29158b894bb707bcfc58eee05
lanelyx <leave.m3@yandex.ru>
2025-10-05 20:05:32 +0300

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,23 +1,30 @@
-import allure
+from typing import Any, Generator
+
 import pytest
 from playwright.sync_api import Page, Playwright
 
 from pages.authentication.registration_page import RegistrationPage
 from _pytest.fixtures import SubRequest
 
+from tools.playwright.pages import initialize_playwright_page
+
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+    #
+    # browser = playwright.chromium.launch(headless=False)
+    # context = browser.new_context(record_video_dir='./videos')
+    # context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    # page = context.new_page()
+    #
+    # yield page
+    #
+    # context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
+    # browser.close()
+    #
+    # allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    # allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
 
 
 @pytest.fixture(scope='session')
@@ -36,13 +43,19 @@ def initialize_browser_state(playwright: Playwright) -> None:
 
 @pytest.fixture
 def chromium_page_with_state(request: SubRequest, playwright: Playwright, initialize_browser_state) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser_state.json')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-
-    yield context.new_page()
-
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state='browser_state.json')
+    # browser = playwright.chromium.launch(headless=False)
+    # context = browser.new_context(storage_state='browser_state.json', record_video_dir='./videos')
+    # context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    # page = context.new_page()
+    #
+    # yield page
+    #
+    # context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
+    # browser.close()
+    #
+    # allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+    # allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
