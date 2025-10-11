88fcf2eac62425713d7cb1267b668d63b67e1611
dontkillmyvibee <lysorryoops@gmail.com>
2025-10-11 19:05:23 +0300

    Практикуемся в интеграции покрытия в UI тесты

--- elements/input.py
+++ elements/input.py
@@ -1,4 +1,5 @@
 import allure
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 from playwright.sync_api import expect, Locator
@@ -16,6 +17,9 @@ class Input(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('input')
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(nth, **kwargs)}//input'
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Filling {self.type_of} "{self.name}" with "{value}"'
 
@@ -24,6 +28,8 @@ class Input(BaseElement):
             logger.info(step)
             locator.fill(value)
 
+        self.track_coverage(action_type=ActionType.FILL, nth=nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
 
@@ -31,3 +37,5 @@ class Input(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             logger.info(step)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(action_type=ActionType.VALUE, nth=nth, **kwargs)
